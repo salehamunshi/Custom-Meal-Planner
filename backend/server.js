3661782298bfd2aa953b676e3362bf5c89ace98b
Saleha <117444346+salehamunshi@users.noreply.github.com>
2025-03-12 16:56:36 +0000

    be: update url for cors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
@@ -10,17 +10,19 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-    origin: 'https://obscure-capybara-r9gjvvqg6vg3xj44-3000.app.github.dev', 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
     credentials: true,
-}));
+  })
+);
 
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
